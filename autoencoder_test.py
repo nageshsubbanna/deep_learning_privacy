5067047c35091da4e4c4794811c8bc7bb830266c
nageshsubbanna <snag1978@yahoo.com>
2021-03-23 08:49:06 -0600

    Update autoencoder_test.py

--- autoencoder_test.py
+++ autoencoder_test.py
@@ -7,7 +7,7 @@ import os
 from keras import backend as K
 K.clear_session()
 
-with h5py.File('object.nii', 'r') as f:
+with nii.File('object.nii', 'r') as f:
     train_data = f['train_mat'][...]
     val_data = f['val_mat'][...]
     test_data = f['test_mat'][...]
