e3a63dac7ddc486707483e8e099f44e25390e734
nageshsubbanna <snag1978@yahoo.com>
2021-03-26 06:44:35 -0600

    Add files via upload

---
+++ autoencoder_train_brain.py
@@ -0,0 +1,114 @@
+from __future__ import print_function
+
+import os
+from skimage.transform import resize
+from skimage.io import imsave
+import numpy as np
+from keras.models import Model
+from keras.layers import Input, concatenate, Conv2D, MaxPooling2D, Conv2DTranspose
+from keras.optimizers import Adam
+from keras.callbacks import ModelCheckpoint
+from keras import backend as K
+
+from data import load_train_data, load_test_data
+
+img_rows=256
+img_cols=256
+img_height=256
+
+smooth=1
+
+def dice_score (img_true, img_pred):
+    img_true_f=K.flatten(img_true)
+    img_pred_f=K.flatten(img_pred)
+
+    intersect=K.sum(img_true_f*img_pred_f)
+    return (2*intersection+smooth)/(K.sum(img_true_f) + K.sum(img_pred_f) + smooth)
+
+
+def dice_coeff_loss (img_true, img_pred):
+    return -dice_coef(y_true, y_pred)
+
+def get_unet():
+    inputs = Input((img_rows, img_cols, 1))
+    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(inputs)
+    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+
+    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(pool1)
+    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+
+    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(pool2)
+    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+
+    conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(pool3)
+    conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv4)
+    pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
+
+    conv5 = Conv2D(512, (3, 3), activation='relu', padding='same')(pool4)
+    conv5 = Conv2D(512, (3, 3), activation='relu', padding='same')(conv5)
+
+    conv6 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv5)
+    conv6 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv6)
+
+    conv7 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv6)
+    conv7 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv7)
+
+    conv8 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv7)
+    conv8 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv8)
+
+    conv9 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv8)
+    conv9 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv9)
+
+    conv10 = Conv2D(1, (1, 1), activation='sigmoid')(conv9)
+
+    model = Model(inputs=[inputs], outputs=[conv10])
+
+    model.compile(optimizer=Adam(lr=1e-5), loss=dice_coef_loss, metrics=[dice_coef])
+
+    return model
+
+
+def train():
+    imgs_train, imgs_mask_train = load_train_data()
+
+    imgs_train = imgs_train.astype('float32')
+    mean = np.mean(imgs_train)  # mean for data centering
+    std = np.std(imgs_train)  # std for data normalization
+
+    imgs_train -= mean
+    imgs_train /= std
+
+    imgs_mask_train = imgs_mask_train.astype('float32')
+    imgs_mask_train /= 255.  # scale masks to [0, 1]
+
+    model = get_unet()
+    model_checkpoint = ModelCheckpoint('weights.h5', monitor='val_loss', save_best_only=True)
+
+    model.fit(imgs_train, imgs_mask_train, batch_size=32, nb_epoch=20, verbose=1, shuffle=True,
+              validation_split=0.2,
+              callbacks=[model_checkpoint])
+
+    imgs_test, imgs_id_test = load_test_data()
+    imgs_test = preprocess(imgs_test)
+
+    imgs_test = imgs_test.astype('float32')
+    imgs_test -= mean
+    imgs_test /= std
+
+    model.load_weights('weights.h5')
+
+    imgs_mask_test = model.predict(imgs_test, verbose=1)
+    np.save('imgs_mask_test.npy', imgs_mask_test)
+
+    pred_dir = 'preds'
+    if not os.path.exists(pred_dir):
+        os.mkdir(pred_dir)
+    for image, image_id in zip(imgs_mask_test, imgs_id_test):
+        image = (image[:, :, 0] * 255.).astype(np.uint8)
+        imsave(os.path.join(pred_dir, str(image_id) + '_pred.png'), image)
+
+if __name__ == '__main__':
+    train()
